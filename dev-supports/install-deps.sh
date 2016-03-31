8c11df6c5cae6922be3d915bf4118616cec246f2
Hao Chen <hchen9@ebay.com>
2016-03-31 09:51:06 +0800

    Remove Logstash in lower case

--- dev-supports/install-deps.sh
+++ dev-supports/install-deps.sh
@@ -3,29 +3,54 @@
 logstash_version=$1
 logstash_package=logstash-all-plugins-${logstash_version}.tar.gz
 logstash_dir=logstash-${logstash_version}
+logstash_download_link="https://download.elastic.co/logstash/logstash/${logstash_package}"
 
 if [ "$2" != "" ] && [ "$2" != "logstorm-parent" ];then
 	exit 0
 fi
 
-cd $(dirname $0)/../
-
-if [ ! -e vendor ];then
-	echo "Create vendor"
-	mkdir vendor
+if [ "$logstash_version" == "" ];then
+    echo "Usage: $0 [logstash-version]" 1>&2
+    exit 1
 fi
 
-if [ -e vendor/${logstash_dir} ];then
-	echo "vendor/${logstash_dir} already exists"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Installing Logstash (version: ${logstash_version})"
+echo "[INFO] ------------------------------------------------------------------------"
+
+cd $(dirname $0)/../
+
+if [ ! -e build ];then
+	echo "[INFO] Create directory 'build'"
+	mkdir -p build
+fi
+
+cd build
+
+if [ -e ${logstash_dir} ];then
+	echo "[INFO] build/${logstash_dir} already exists"
 	exit 0
 fi
 
-cd vendor
+$(which wget 1>/dev/null 2>&1)
+if [ "$?" != "0" ];then
+    echo "[ERROR] Failed to install logstash because command 'wget'(https://www.gnu.org/software/wget/) is not found" 1>&2
+    echo "[ERROR] To resolve the problem, you could install 'wget' which is compatible with your operation system, "
+    echo "[ERROR] or download logstash from ${logstash_download_link} and unpackage into `pwd`/${logstash_dir} manually" 1>&2
+    exit 1
+fi
 
-if [ ! -e vendor/${logstash_package} ];then
-	wget https://download.elastic.co/logstash/logstash/${logstash_package}
+if [ ! -e ${logstash_package} ];then
+    echo "[INFO] Installing ${logstash_download_link} into build/${logstash_dir}"
+	wget ${logstash_download_link}
 fi
 
 tar xzvf ${logstash_package}
 
-exit 0
+if [ -e ${logstash_dir} ];then
+    echo "[INFO] Successfully installed logstash into build/${logstash_dir}"
+    exit 0
+else
+    echo "[ERROR] Failed to install logstash, because build/${logstash_dir} not exist"
+    exit 1
+fi
