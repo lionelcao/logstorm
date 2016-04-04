7c0bc4abe8766ee2f643dba14de6cc1802cc4cc4
Hao Chen <hchen9@ebay.com>
2016-04-05 00:34:04 +0800

    Add default static index.html and move templated ui into /ui

--- server/src/main/java/com/ebay/logstorm/server/controllers/UIController.java
+++ server/src/main/java/com/ebay/logstorm/server/controllers/UIController.java
@@ -24,7 +24,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/")
 public class UIController {
-    @RequestMapping
+    @RequestMapping("/ui")
     public ModelAndView home(){
         return listPipelines();
     }
