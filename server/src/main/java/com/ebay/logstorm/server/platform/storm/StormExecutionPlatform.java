77f9631e9cb5a31af4bb297c82462374a3b2459b
Hao Chen <hao@apache.org>
2016-04-29 23:48:02 +0800

    Submit storm topology in remote mode

--- server/src/main/java/com/ebay/logstorm/server/platform/storm/StormExecutionPlatform.java
+++ server/src/main/java/com/ebay/logstorm/server/platform/storm/StormExecutionPlatform.java
@@ -1,5 +1,11 @@
 package com.ebay.logstorm.server.platform.storm;
 
+import backtype.storm.generated.Nimbus;
+import backtype.storm.generated.NotAliveException;
+import backtype.storm.generated.TopologyInfo;
+import backtype.storm.generated.TopologySummary;
+import backtype.storm.utils.NimbusClient;
+import backtype.storm.utils.Utils;
 import com.ebay.logstorm.core.LogStormConstants;
 import com.ebay.logstorm.core.PipelineContext;
 import com.ebay.logstorm.core.compiler.Pipeline;
@@ -11,9 +17,11 @@ import com.ebay.logstorm.server.platform.ExecutionManager;
 import com.ebay.logstorm.server.platform.ExecutionPlatform;
 import com.ebay.logstorm.server.platform.TaskExecutor;
 import com.typesafe.config.ConfigFactory;
+import org.apache.thrift7.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -34,13 +42,16 @@ import java.util.Properties;
  */
 public class StormExecutionPlatform implements ExecutionPlatform {
     private final static Logger LOG = LoggerFactory.getLogger(StormExecutionPlatform.class);
-
     private StormPipelineRunner runner;
-    @Override
-    public void prepare(Properties properties) {
+
+    static {
         String pipelineJar = ConfigFactory.load().getString("pipeline.jar");
         LOG.info("Setting storm.jar as {}",pipelineJar);
         System.setProperty("storm.jar",pipelineJar);
+    }
+
+    @Override
+    public void prepare(Properties properties) {
         runner = new StormPipelineRunner();
     }
 
@@ -63,6 +74,7 @@ public class StormExecutionPlatform implements ExecutionPlatform {
                 entity.setUrl("/api/executor/" + entity.getName());
             case CLUSTER:
                 startInRemoteMode(pipeline);
+                status(entity);
          //   throw new IllegalStateException("State of "+entity.getPipeline().getName()+" is illegal: "+ entity.getPipeline().getMode());
         }
     }
@@ -96,6 +108,42 @@ public class StormExecutionPlatform implements ExecutionPlatform {
                 LOG.info("Status of pipeline: {} changed from {} to {}", entity, currentStatus, newStatus);
                 entity.setStatus(newStatus);
                 entity.setDescription(String.format("Status of pipeline: %s changed from %s to %s", entity, currentStatus, newStatus));
+            }
+        } else {
+            try {
+                String topology_id_key = "topology.id";
+                Map clusterConf = Utils.readStormConfig();
+                clusterConf.putAll(Utils.readCommandLineOpts());
+                Nimbus.Client client = NimbusClient.getConfiguredClient(clusterConf).getClient();
+
+                String id = entity.getProperties() == null? null:entity.getProperties().getProperty(topology_id_key);
+
+                if(id == null) {
+                    for (TopologySummary topologySummary : client.getClusterInfo().get_topologies()) {
+                        if (topologySummary.get_name().equals(entity.getPipeline().getName())) {
+                            id = topologySummary.get_id();
+                        }
+                    }
+                }
+
+                if(id == null){
+                    throw new IllegalStateException("Topology named "+entity.getPipeline().getName()+" is not found");
+                } else {
+                    TopologyInfo topologyInfo = client.getTopologyInfo(id);
+                    entity.setProperty(topology_id_key, topologyInfo.get_id());
+                    entity.setProperty("topology.name", topologyInfo.get_name());
+                    entity.setProperty("topology.status", topologyInfo.get_status());
+                    entity.setProperty("topology.uptime_secs", String.valueOf(topologyInfo.get_uptime_secs()));
+                    entity.setProperty("topology.executors_size", String.valueOf(topologyInfo.get_executors_size()));
+                    entity.setProperty("topology.errors_size", String.valueOf(topologyInfo.get_errors_size()));
+                    entity.setName(topologyInfo.get_id());
+                }
+            }catch (NotAliveException ex){
+                LOG.error("{} not alive",entity.getPipeline().getName(),ex);
+                entity.setStatus(PipelineExecutionStatus.STOPPED);
+            } catch (TException ex ){
+                LOG.error("Failed to connect to nimbus through thrift",ex);
+                throw ex;
             }
         }
     }
