330cefe89f70ca2ddd9a8accba09c651408e7a31
Hao Chen <hao@apache.org>
2016-05-01 00:32:23 +0800

    Add LogStormServerDebug for easily debug

--- server/src/main/java/com/ebay/logstorm/server/utils/LogStormServerDebug.java
+++ server/src/main/java/com/ebay/logstorm/server/utils/LogStormServerDebug.java
@@ -1,15 +1,3 @@
-package com.ebay.logstorm.server;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.TrueFileFilter;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.util.Collection;
-
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -26,28 +14,50 @@ import java.util.Collection;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-public class LogStormServerTest {
-    private final static Logger LOG = LoggerFactory.getLogger(LogStormServerTest.class);
+package com.ebay.logstorm.server.utils;
+
+import com.ebay.logstorm.server.LogStormServer;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Collection;
+
+
+/**
+ * Used for dev/debug mode only with source code
+ *
+ * 1. Automatically load jar path from building target
+ */
+public class LogStormServerDebug {
+    private final static Logger LOG = LoggerFactory.getLogger(LogStormServerDebug.class);
     private static File findAssemblyJarFile(){
-        String projectRootDir = System.getProperty("user.dir")+"/../";
+        String projectRootDir = System.getProperty("user.dir");
         String assemblyModuleTargeDirPath=projectRootDir+"/assembly/target/";
         File assemblyTargeDirFile = new File(assemblyModuleTargeDirPath);
         if(!assemblyTargeDirFile.exists()) {
             throw new IllegalStateException(assemblyModuleTargeDirPath + " not found, please execute 'mvn install -DskipTests' under " + projectRootDir + " to build the project firstly and retry");
         }
         String jarFileNameWildCard="logstorm-assembly-*.jar";
-        Collection<File> jarFiles = FileUtils.listFiles(assemblyTargeDirFile,new WildcardFileFilter(jarFileNameWildCard),TrueFileFilter.INSTANCE);
+        Collection<File> jarFiles = FileUtils.listFiles(assemblyTargeDirFile,new WildcardFileFilter(jarFileNameWildCard), TrueFileFilter.INSTANCE);
         if(jarFiles.size() == 0){
-            throw new IllegalStateException("jar is not found, please execute 'mvn install -DskipTests' under " + projectRootDir + " to build the project firstly and retry");
+            throw new IllegalStateException("jar is not found, please execute 'mvn install -DskipTests' from project root firstly and retry");
         }
         File jarFile = jarFiles.iterator().next();
-        LOG.info("Found pipeline.jar: {}",jarFile.getAbsolutePath());
+        LOG.debug("Found pipeline.jar: {}",jarFile.getAbsolutePath());
         return jarFile;
     }
 
-    @Test
-    public void testStartServer(){
-        System.setProperty("storm.jar",findAssemblyJarFile().getAbsolutePath());
-        LogStormServer.start(new String[]{});
+    static {
+        String path = findAssemblyJarFile().getAbsolutePath();
+        LOG.info("pipeline.jar: {}",path);
+        System.setProperty("pipeline.jar",path);
+    }
+
+    public static void main(String[] args){
+        LogStormServer.start(args);
     }
 }
