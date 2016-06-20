e9513b6c6be80dfc53eb13af3d82d4348df8f4ba
jiljiang <jiljiang@ebay.com>
2016-06-20 16:45:45 +0800

    refact web

--- server/src/main/webapp/app/management/management.ctrl.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    angular.module("app").controller("ManagementController",ManagementController);
-    ManagementController.$inject=["$scope","$log"];
-
-    function ManagementController($scope,$log){
-        $log.info("Loading management");
-    }
-})();
---
+++ server/src/main/webapp/app/public/js/srv/api.js
@@ -0,0 +1,31 @@
+(function() {
+	'use strict';
+
+	var host  = "http://localhost:8080/";
+	/* App Module */
+	var logStormApp = angular.module('logStormApp');
+
+	logStormApp.service('API', function($http) {
+		return {
+			get: function (url) {
+				var list = [];
+				list._promise = $http.get(host + url).then(function (res) {
+					var result = res.data;
+					list.splice(0);
+					Array.prototype.push.apply(list, result.data);
+				});
+				return list;
+			},
+			post: function (url, data) {
+				return $http.post(host + url, data);
+			},
+			delete: function (url, data) {
+				if(typeof data === "string") {
+					return $http.delete(host + url + "/" + data);
+				} else {
+					return $http.delete(host + url, {data: data});
+				}
+			}
+		};
+	});
+}());
